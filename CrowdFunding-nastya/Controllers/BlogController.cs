d67b6fee5e5490b8586e81c17a5cfd2631aede3c
Ahsan Shafique <62590318+Ahsan-803@users.noreply.github.com>
2024-01-02 20:27:01 +0500

    AhsanChanging

--- CrowdFunding-nastya/Controllers/BlogController.cs
+++ CrowdFunding-nastya/Controllers/BlogController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CrowdFunding_nastya.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,13 +12,175 @@ namespace CrowdFunding_nastya.Controllers
 {
     public class BlogController : Controller
     {
+        CrowdfundingnastyaEntities _dbContext = new CrowdfundingnastyaEntities();
         // GET: Blog
         public ActionResult Index()
         {
-            return View();
-        } 
-        public ActionResult add()
+            var blogUser = _dbContext.tblBlogs.ToList();
+            return View(blogUser);
+        }
+
+        [HttpGet]
+        public ActionResult add(int id = 0) 
         {
+            var priorityList = _dbContext.tblBlogPriorities.ToList();
+            ViewBag.PriorityList = new SelectList(priorityList, "PriorityId", "Priority");
+            var categoryList = _dbContext.tblBlogCategories.ToList();
+            ViewBag.CategoryList = new SelectList(categoryList, "CategoryId", "CategoryName");
+            var blogTypeList = _dbContext.tblBlogTypes.ToList();
+            ViewBag.BlogTypeList = new SelectList(blogTypeList, "BlogTypeId", "BlogTypeName");
+
+            if (id > 0)
+            {
+                // Edit operation
+
+                var blogToEdit = _dbContext.tblBlogs.FirstOrDefault(model => model.BlogId == id);
+                return View(blogToEdit);
+            }
+            else
+            {
+                // Add operation
+                return View(new tblBlog());
+            }
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult add(tblBlog tblBlog)
+        {
+            if (tblBlog.BlogId > 0)
+            {
+                // Update operation
+                var existingBlog = _dbContext.tblBlogs.FirstOrDefault(model => model.BlogId == tblBlog.BlogId);
+                if (existingBlog != null)
+                {
+                    // Update existing blog properties here
+                    if (tblBlog.BlogThumbnailImage != null)
+                    {
+                        string fileName = Path.GetFileNameWithoutExtension(tblBlog.BlogThumbnailImage.FileName);
+                        string extension = Path.GetExtension(tblBlog.BlogThumbnailImage.FileName);
+                        fileName = fileName + extension;
+                        tblBlog.ThumbnailImage = "/assets/assets/img/" + fileName;
+                        fileName = Path.Combine(Server.MapPath("/assets/assets/img/"), fileName);
+                        tblBlog.BlogThumbnailImage.SaveAs(fileName);
+                    }
+
+                    if (tblBlog.BlogAttachedFiles != null && tblBlog.BlogAttachedFiles.Count > 0)
+                    {
+                        foreach (var file in tblBlog.BlogAttachedFiles)
+                        {
+                            if (file != null && file.ContentLength > 0)
+                            {
+                                string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                                string extension = Path.GetExtension(file.FileName);
+                                fileName = fileName + extension;
+
+                                string filePath = "/assets/assets/img/" + fileName;
+                                fileName = Path.Combine(Server.MapPath("/assets/assets/img/"), fileName);
+                                file.SaveAs(fileName);
+
+                                // Assuming _dbContextContext is your Entity Framework _dbContextContext
+                                var attachment = new tblBlogAttachedFile
+                                {
+                                    BlogId = tblBlog.BlogId, // Assuming you have the BlogID available
+                                    AttachedFiles = filePath,
+                                    //FileName = fileName,
+                                    //ContentType = file.ContentType,
+                                    // ... other properties you may want to store
+                                };
+
+                                _dbContext.tblBlogAttachedFiles.AddOrUpdate(attachment);
+
+                            }
+                        }
+                    }
+
+                    existingBlog.BlogTitle = tblBlog.BlogTitle;
+                    existingBlog.BlogDescription = tblBlog.BlogDescription;
+                    existingBlog.ThumbnailImage = tblBlog.ThumbnailImage;
+                    existingBlog.PriorityId = tblBlog.PriorityId;
+                    existingBlog.IsActive = tblBlog.IsActive;
+                    existingBlog.CategoryId = tblBlog.CategoryId;
+                    existingBlog.BlogTypeId = tblBlog.BlogTypeId;
+
+                    _dbContext.tblBlogs.AddOrUpdate(existingBlog);
+                    _dbContext.SaveChanges();
+                    return RedirectToAction("Index", "Blog");
+                }
+            }
+            else
+            {
+                // Add operation
+                if (tblBlog.BlogThumbnailImage != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(tblBlog.BlogThumbnailImage.FileName);
+                    string extension = Path.GetExtension(tblBlog.BlogThumbnailImage.FileName);
+                    fileName = fileName + extension;
+                    tblBlog.ThumbnailImage = "/assets/assets/img/" + fileName;
+                    fileName = Path.Combine(Server.MapPath("/assets/assets/img/"), fileName);
+                    tblBlog.BlogThumbnailImage.SaveAs(fileName);
+                }
+
+                if (tblBlog.BlogAttachedFiles != null && tblBlog.BlogAttachedFiles.Count > 0)
+                {
+                    foreach (var file in tblBlog.BlogAttachedFiles)
+                    {
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                            string extension = Path.GetExtension(file.FileName);
+                            fileName = fileName + extension;
+
+                            string filePath = "/assets/assets/img/" + fileName;
+                            fileName = Path.Combine(Server.MapPath("/assets/assets/img/"), fileName);
+                            file.SaveAs(fileName);
+
+                            // Assuming _dbContextContext is your Entity Framework _dbContextContext
+                            var attachment = new tblBlogAttachedFile
+                            {
+                                BlogId = tblBlog.BlogId, // Assuming you have the BlogID available
+                                AttachedFiles = filePath,
+                                //FileName = fileName,
+                                //ContentType = file.ContentType,
+                                // ... other properties you may want to store
+                            };
+
+                            _dbContext.tblBlogAttachedFiles.Add(attachment);
+
+                        }
+                    }
+                }
+
+                //tblBlog.CreatedDate = DateTime.Now;
+                _dbContext.tblBlogs.Add(tblBlog);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index", "Blog");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id) 
+        {
+            var checkUser = _dbContext.tblBlogs.Where(model => model.BlogId.Equals(id)).FirstOrDefault();
+            if(checkUser != null) 
+            {
+                _dbContext.Entry(checkUser).State = EntityState.Modified;
+                int a = _dbContext.SaveChanges();
+                if(a > 0) 
+                {
+                    return Json(new { success = true });
+                }
+            }
+            else 
+            {  
+                return Json(new { success = false }); 
+            }
             return View();
         }
     }
--- CrowdFunding-nastya/Controllers/HomeController.cs
+++ CrowdFunding-nastya/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CrowdFunding_nastya.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +12,7 @@ namespace CrowdFunding_nastya.Controllers
 {
     public class HomeController : Controller
     {
+        CrowdfundingnastyaEntities _dbContext = new CrowdfundingnastyaEntities();
         public ActionResult Index()
         {
             return View();
@@ -29,17 +34,145 @@ namespace CrowdFunding_nastya.Controllers
         {
             return View();
         }
-        public ActionResult career()
+        [HttpGet]
+        public ActionResult careerList()
+        {
+            var careerList = _dbContext.tblCareers.ToList();
+            return View(careerList);
+        }
+
+        [HttpGet]
+        public ActionResult career(int id = 0)
+        {
+            var priorityList = _dbContext.tblCareerPriorities.ToList();
+            ViewBag.PriorityList = new SelectList(priorityList, "PriorityId", "PriorityName");
+            var categoryList = _dbContext.tblCareerCategories.ToList();
+            ViewBag.CategoryList = new SelectList(categoryList, "CategoryId", "CategoryName");
+            var blogTypeList = _dbContext.tblCareerBlogTypes.ToList();
+            ViewBag.CareerTypeList = new SelectList(blogTypeList, "BlogTypeId", "BlogTypeName");
+
+            if (id > 0)
+            {
+                // Edit operation
+
+                var CareerToEdit = _dbContext.tblCareers.FirstOrDefault(model => model.CareerId == id);
+                return View(CareerToEdit);
+            }
+            else
+            {
+                // Add operation
+                return View(new tblCareer());
+            }
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Career(tblCareer tblCareer)
+        {
+            if (tblCareer.CareerId > 0)
+            {
+                // Update operation
+                var existingBlog = _dbContext.tblCareers.FirstOrDefault(model => model.CareerId == tblCareer.CareerId);
+                if (existingBlog != null)
+                {
+                    // Update existing blog properties here
+                    if (tblCareer.ThumbnailImage != null)
+                    {
+                        string fileName = Path.GetFileNameWithoutExtension(tblCareer.ThumbnailImage.FileName);
+                        string extension = Path.GetExtension(tblCareer.ThumbnailImage.FileName);
+                        fileName = fileName + extension;
+                        tblCareer.CareerThumbnailImage = "/assets/assets/img/" + fileName;
+                        fileName = Path.Combine(Server.MapPath("/assets/assets/img/"), fileName);
+                        tblCareer.ThumbnailImage.SaveAs(fileName);
+                    }
+
+                    existingBlog.CareerTitle = tblCareer.CareerTitle;
+                    existingBlog.CareerDescription = tblCareer.CareerDescription;
+                    existingBlog.CareerThumbnailImage = tblCareer.CareerThumbnailImage;
+                    existingBlog.PriorityId = tblCareer.PriorityId;
+                    existingBlog.IsActive = tblCareer.IsActive;
+                    existingBlog.CategoryId = tblCareer.CategoryId;
+                    existingBlog.BlogTypeId = tblCareer.BlogTypeId;
+
+                    _dbContext.tblCareers.AddOrUpdate(existingBlog);
+                    _dbContext.SaveChanges();
+                    return RedirectToAction("careerList", "Home");
+                }
+            }
+            else
+            {
+                // Add operation
+                if (tblCareer.ThumbnailImage != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(tblCareer.ThumbnailImage.FileName);
+                    string extension = Path.GetExtension(tblCareer.ThumbnailImage.FileName);
+                    fileName = fileName + extension;
+                    tblCareer.CareerThumbnailImage = "/assets/assets/img/" + fileName;
+                    fileName = Path.Combine(Server.MapPath("/assets/assets/img/"), fileName);
+                    tblCareer.ThumbnailImage.SaveAs(fileName);
+                }
+
+                 tblCareer.CreatedDate = DateTime.Now;
+                _dbContext.tblCareers.Add(tblCareer);
+                _dbContext.SaveChanges();
+                return RedirectToAction("careerList", "Home");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult careerDelete()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult careerDelete(int CareerId)
+        {
+            var checkCareer = _dbContext.tblBlogs.Where(model => model.BlogId.Equals(CareerId)).FirstOrDefault();
+            if (checkCareer != null)
+            {
+                _dbContext.Entry(checkCareer).State = EntityState.Modified;
+                int a = _dbContext.SaveChanges();
+                if (a > 0)
+                {
+                    return Json(new { success = true });
+                }
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult careerAllList()
+        {
+            var allCareer = _dbContext.tblCareers.ToList();
+            return View(allCareer);
+        }
+        [HttpGet]
+        public ActionResult careerDetail(int id)
+        {
+            var careerPre = _dbContext.tblCareers.SingleOrDefault(model => model.CareerId == id);
+            if (careerPre != null)
+            {
+                return View(careerPre);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
         public ActionResult blogs()
         {
-            return View();
+            var blogs = _dbContext.tblBlogs.ToList();
+            return View(blogs);
         }
-        public ActionResult blog_detail()
+        public ActionResult blog_detail(int id)
         {
-            return View();
+            var blogsPre = _dbContext.tblBlogs.SingleOrDefault(model => model.BlogId == id);
+            if (blogsPre != null)
+            {
+                return View(blogsPre);
+            }
+            return RedirectToAction("Index", "Home");
         }
         public ActionResult cases()
         {
