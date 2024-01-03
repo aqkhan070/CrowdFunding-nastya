ac4c2c59151d02b0a589e50efdf55a087f3e770e
Abdul <abdulqadeerkhan070@gmail.com>
2024-01-03 14:51:18 +0500

    Abdul Changes

--- CrowdFunding-nastya/Controllers/HomeController.cs
+++ CrowdFunding-nastya/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CrowdFunding_nastya.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +11,7 @@ namespace CrowdFunding_nastya.Controllers
 {
     public class HomeController : Controller
     {
+        CrowdfundingnastyaEntities dbEntities = new CrowdfundingnastyaEntities();
         public ActionResult Index()
         {
             return View();
@@ -20,14 +24,16 @@ namespace CrowdFunding_nastya.Controllers
         {
             return View();
         }
-    
+
         public ActionResult privacy()
         {
-            return View();
+            tblEditPage sa = dbEntities.tblEditPages.Find(1);
+            return View(sa);
         }
         public ActionResult legal()
         {
-            return View();
+            tblEditPage sa = dbEntities.tblEditPages.Find(1);
+            return View(sa);
         }
         public ActionResult career()
         {
@@ -45,7 +51,7 @@ namespace CrowdFunding_nastya.Controllers
         {
             return View();
         }
-        public ActionResult case_detail ()
+        public ActionResult case_detail()
         {
             return View();
         }
@@ -55,16 +61,109 @@ namespace CrowdFunding_nastya.Controllers
         }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            tblEditPage sa = dbEntities.tblEditPages.Find(1);
+            return View(sa);
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            tblEditPage sa = dbEntities.tblEditPages.Find(1);
+            return View(sa);
         }
+        [HttpPost]
+        public ActionResult Contact(string email, string name, string subject, string message, string phone)
+        {
+            try
+            {
+
+                tblContact contact = new tblContact();
+                contact.Email = email;
+                contact.Name = name;
+                contact.Message = message;
+                contact.Subject = subject;
+                contact.Subject = phone;
+                dbEntities.tblContacts.Add(contact);
+                dbEntities.SaveChanges();
+                return Json(new { status = 1 });
+
+                tblSetting sa = dbEntities.tblSettings.Find(1);
+                using (MailMessage mm = new MailMessage(sa.Email, sa.AdminEmail))
+                {
+
+                    mm.Subject = subject;
+                    string body = "Hello,";
+                    body += "<br /><br />You received a message from " + name + "(" + email + ")";
+                    body += "<br /><br />Phone Number: " + phone + "<br /><br />";
+                    body += message;
+                    mm.Body = body;
+                    mm.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = sa.SMTP;
+                    smtp.EnableSsl = Convert.ToBoolean(sa.isActive);
+                    NetworkCredential NetworkCred = new NetworkCredential(sa.Email, sa.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = Convert.ToInt32(sa.Port);
+                    smtp.Send(mm);
+                }
+                using (MailMessage mm = new MailMessage(sa.Email, email))
+                {
+                    mm.Subject = "Thank you";
+                    string body1 = "";
+                    body1 += "Thank you for contacting us.";
+                    body1 += "We will entertain your concern soon.";
+                    body1 += "<br /><br />Yours,<br />The Business Shower Team";
+
+                    string body = "";
+                    body += "<body  style='background-color:white !important'>";
+                    body += " <div>";
+                    //body += "<h3>Hello " + sa.ReceiveName + ",</h3>";
+                    body += " <table style='background-color: #f2f3f8; max-width:670px;' width='100%' border='0'  cellpadding='0' cellspacing='0'>";
+                    body += " <tbody> <tr style='background-color:rgba(40, 58, 90, 0.9);'><td style='padding: 0 35px; background-color:rgba(40, 58, 90, 0.9);'><a><h1 style ='color:white' > 21Drop </h1>   </a></td> </tr>";
+                    body += "<tr style='color:#455056; font-size:15px;line-height:35px;text-align: center;'><td style='padding:6px;text-align: center;'> <b>Hello " + name + ",</b></td></tr><tr style='color:#455056; font-size:15px;line-height:35px;'><td style='padding:6px;text-align: center;'>" + body1 + "</td></tr>";
+                    body += "  </tbody></table>";
+                    body += "</body>";
+
+                    mm.Body = body;
+                    mm.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = sa.SMTP;
+                    smtp.EnableSsl = Convert.ToBoolean(sa.isActive);
+                    NetworkCredential NetworkCred = new NetworkCredential(sa.Email, sa.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = Convert.ToInt32(sa.Port);
+                    smtp.Send(mm);
+                }
+                return Json(new { status = 1 });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = -1, data = ex.Message });
+                throw;
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Subscribe(string email, string name)
+        {
+            try
+            {
+
+                tblSubscriber contact = new tblSubscriber();
+                contact.Email = email;
+                contact.SubsName = name;
+                dbEntities.tblSubscribers.Add(contact);
+                dbEntities.SaveChanges();
+                return Json(new { status = 1 });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = -1, data = ex.Message });
+                throw;
+            }
+        } 
     }
 }
--- CrowdFunding-nastya/Controllers/SettingController.cs
+++ CrowdFunding-nastya/Controllers/SettingController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CrowdFunding_nastya.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,183 @@ namespace CrowdFunding_nastya.Controllers
 {
     public class SettingController : Controller
     {
-        // GET: Setting
+        CrowdfundingnastyaEntities dbEntities = new CrowdfundingnastyaEntities();
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                tblSetting sa = dbEntities.tblSettings.Find(1);
+                ViewBag.message = null;
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
         }
+        [HttpPost]
+        public ActionResult Index(tblSetting setting)
+        {
+            try
+            {
+                dbEntities.Entry(setting).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+                ViewBag.message = "Record successfully updated.";
+                tblSetting sa = dbEntities.tblSettings.Find(2);
+                //System.Configuration.Configuration objConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~");
+                //PayPal.SDKConfigHandler objAppsettings = (PayPal.SDKConfigHandler)objConfig.GetSection("paypal");
+                ////Edit
+                //if (objAppsettings != null)
+                //{
+                //    objAppsettings.Settings["mode"].Value = setting.AccountType.ToString();
+                //    objAppsettings.Settings["clientId"].Value = setting.clientId.ToString();
+                //    objAppsettings.Settings["clientSecret"].Value = setting.clientSecret.ToString();
+                //    objConfig.Save();
+                //}
+
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        public ActionResult Contact(string lng = "en")
+        {
+            try
+            {
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                ViewBag.message = null;
+                ViewBag.lng = lng;
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        [HttpPost]
+        public ActionResult Contact(tblEditPage setting)
+        {
+            try
+            {
+                tblEditPage page = dbEntities.tblEditPages.Find(1);
+                page.Contact = setting.Contact.Replace("'", "");
+                
+
+                page.Email = setting.Email.Replace("'", "");
+                page.Phone = setting.Phone.Replace("'", "");
+                page.Address = setting.Address.Replace("'", "");
+                page.Facebook = setting.Facebook.Replace("'", "");
+                page.Instagram = setting.Instagram.Replace("'", "");
+                page.Twitter = setting.Twitter.Replace("'", "");
+                page.Linkdin = setting.Linkdin.Replace("'", "");
+                page.Google = setting.Google.Replace("'", "");
+                dbEntities.Entry(page).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+                ViewBag.message = "Record successfully updated.";
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        public ActionResult About()
+        {
+            try
+            {
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                if (sa == null)
+                {
+                    sa = new tblEditPage();
+                }
+                ViewBag.message = null;
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        [HttpPost]
+        public ActionResult About(tblEditPage setting)
+        {
+            try
+            {
+                tblEditPage page = dbEntities.tblEditPages.Find(1);
+                dbEntities.Entry(page).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+                ViewBag.message = "Record successfully updated.";
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        public ActionResult Privacy()
+        {
+            try
+            {
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                ViewBag.message = null;
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        [HttpPost]
+        public ActionResult Privacy(tblEditPage setting)
+        {
+            try
+            {
+                tblEditPage page = dbEntities.tblEditPages.Find(1);
+                dbEntities.Entry(page).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+                ViewBag.message = "Record successfully updated.";
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        public ActionResult Legal()
+        {
+            try
+            {
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                ViewBag.message = null;
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+        [HttpPost]
+        public ActionResult Legal(tblEditPage setting, string lng)
+        {
+            try
+            {
+                tblEditPage page = dbEntities.tblEditPages.Find(1);
+                dbEntities.Entry(page).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+                ViewBag.message = "Record successfully updated.";
+                tblEditPage sa = dbEntities.tblEditPages.Find(1);
+                return View(sa);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error", new { message = ex.Message });
+            }
+        }
+
     }
 }
