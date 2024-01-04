e61b46dd69efb97a6ab99c9ef2cc263d50776f90
Abdul <abdulqadeerkhan070@gmail.com>
2024-01-04 12:04:14 +0500

    Action page

---
+++ CrowdFunding-nastya/Controllers/ActionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrowdFunding_nastya.Controllers
+{
+    public class ActionController : Controller
+    {
+        // GET: Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
